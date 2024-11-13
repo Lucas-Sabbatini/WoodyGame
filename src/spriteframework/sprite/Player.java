9e43c77faf0691d76d163e94dc95e08d42ec21a2
Lucas Sabbatini Janot <lucassabbatinijp@gmail.com>
2024-11-13 16:18:34 -0600

    :sparkles: Initial Commit

---
+++ src/spriteframework/sprite/Player.java
@@ -0,0 +1,80 @@
+package spriteframework.sprite;
+
+import javax.swing.ImageIcon;
+
+import spriteframework.Commons;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+public class Player extends Sprite {
+
+    private int width;
+
+    public Player() {
+        loadImage();
+		getImageDimensions();
+		resetState();
+    }
+
+    protected void loadImage() {
+        String imagePath = "/images/player.png"; // Update this path as needed
+        java.net.URL imgURL = getClass().getResource(imagePath);
+        if (imgURL != null) {
+            ImageIcon ii = new ImageIcon(imgURL);
+            setImage(ii.getImage());
+        } else {
+            System.err.println("Couldn't find file: " + imagePath);
+        }
+    }
+    
+    public void act() {
+
+        x += dx;
+
+        if (x <= 2) {
+
+            x = 2;
+        }
+
+        if (x >= Commons.BOARD_WIDTH - 2 * width) {
+
+            x = Commons.BOARD_WIDTH - 2 * width;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+
+            dx = -2;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+
+            dx = 2;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+
+            dx = 0;
+        }
+    }
+    private void resetState() {
+
+        setX(Commons.INIT_PLAYER_X);
+        setY(Commons.INIT_PLAYER_Y);
+    }
+}
